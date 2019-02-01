325cfda5dd322a14c3b0b7d724e005117e13b71b
Steven Zgaljic <szgaljic@gmail.com>
2019-02-01 14:20:08 -0600

    init aws and h2 for test

--- src/main/resources/db/migration/V1.0__users.sql
+++ src/main/resources/db/migration/V1.0__users.sql
@@ -1,39 +1,38 @@
 -- https://docs.spring.io/spring-security/site/docs/current/reference/htmlsingle/#user-schema
 
-create table users(
+create table `users` (
   username varchar(50) not null primary key,
   password varchar(60) not null,
   enabled boolean not null
 );
 
-create table authorities (
+create table `authorities` (
   username varchar(50) not null,
   authority varchar(50) not null,
   constraint fk_authorities_users foreign key(username) references users(username)
 );
 create unique index ix_auth_username on authorities (username,authority);
 
-create table groups (
+create table `user_groups` (
   id bigint auto_increment primary key,
   group_name varchar(50) not null
 );
 
-create table group_authorities (
+create table `user_group_authorities` (
   group_id bigint not null,
   authority varchar(50) not null,
-  constraint fk_group_authorities_group foreign key(group_id) references groups(id)
+  constraint fk_group_authorities_group foreign key(group_id) references user_groups(id)
 );
 
-create table group_members (
+create table `user_group_members` (
   id bigint auto_increment primary key,
   username varchar(50) not null,
   group_id bigint not null,
-  constraint fk_group_members_group foreign key(group_id) references groups(id)
+  constraint fk_group_members_group foreign key(group_id) references user_groups(id)
 );
 
--- default password is 'pass'
-insert into users(username, password, enabled) values
+insert into `users` (username, password, enabled) values
   ('admin', '$2a$12$AcPJ5D0I1XXvSjDWgZGO4OJ9x33VRxPy/BqtNLe.pOaUmZpMD2EK.', true);
 
-insert into authorities (username, authority) values
+insert into `authorities` (username, authority) values
   ('admin', 'ROLE_ADMIN');
