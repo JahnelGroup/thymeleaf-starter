83a2020d636d349ebd3601818989dd61a40b71c9
Steven Zgaljic <szgaljic@gmail.com>
2019-02-03 13:47:23 -0600

    ready for eb

---
+++ src/main/kotlin/com/jahnelgroup/start/service/context/UserContextServiceImpl.kt
@@ -0,0 +1,25 @@
+package com.jahnelgroup.start.service.context
+
+import com.jahnelgroup.start.domain.user.User
+import com.jahnelgroup.start.domain.user.UserRepo
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserContextServiceImpl(private val userRepo: UserRepo) : UserContextService {
+
+    override fun currentUsername(): String {
+        val username = SecurityContextHolder.getContext().authentication.name
+        return username ?: throw RuntimeException("UnauthenticatedException")
+    }
+
+    override fun currentUser(): User =
+            userRepo.findByUsername(currentUsername()).get()
+
+    override fun currentAuthorities(): Set<String> {
+        var user = SecurityContextHolder.getContext().authentication.principal as
+                org.springframework.security.core.userdetails.User
+        return user.authorities.map{ it.authority }.toSet()
+    }
+
+}
