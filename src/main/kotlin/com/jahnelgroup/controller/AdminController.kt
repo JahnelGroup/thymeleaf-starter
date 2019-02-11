b211a5201b7345509c6b4d5de7eb138357a9e7b2
Steven Zgaljic <szgaljic@gmail.com>
2019-02-10 21:02:26 -0600

    init create user form

--- src/main/kotlin/com/jahnelgroup/controller/AdminController.kt
+++ src/main/kotlin/com/jahnelgroup/controller/AdminController.kt
@@ -1,14 +1,23 @@
 package com.jahnelgroup.controller
 
+import com.jahnelgroup.domain.user.User
 import com.jahnelgroup.domain.user.UserRepo
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class AdminController(private var userRepo: UserRepo){
 
+    @ModelAttribute("createUser")
+    fun emptyCreateUser() = User()
+
+    /**
+     * Admin searching for users in the system.
+     */
     @GetMapping("/admin/users")
     fun users(model: Model, @RequestParam inputSearch: String?): String {
         if(inputSearch != null){
@@ -16,6 +25,18 @@ class AdminController(private var userRepo: UserRepo){
             model.addAttribute("inputSearch", inputSearch)
         }
         return "layouts/admin/users"
+    }
+
+    /**
+     * Admin creating a new user.
+     */
+    @PostMapping("/admin/user")
+    fun createUser(model: Model, @ModelAttribute("createUser") user: User?){
+        if( user != null ){
+            user.enabled = true
+            // TODO: Use Password Encoder
+            userRepo.save(user)
+        }
     }
 
     @GetMapping("/admin/groups")
--- src/main/kotlin/com/jahnelgroup/domain/user/User.kt
+++ src/main/kotlin/com/jahnelgroup/domain/user/User.kt
@@ -8,21 +8,21 @@ import javax.persistence.*
 data class User (
 
     @field:Id
-    var username: String,
+    var username: String? = null,
 
-    var firstName: String,
+    var firstName: String? = null,
 
-    var lastName: String,
+    var lastName: String? = null,
 
-    var email: String,
-
-    @get:JsonIgnore
-    var password: String,
+    var email: String? = null,
 
     @get:JsonIgnore
-    var enabled: Boolean,
+    var password: String? = null,
+
+    @get:JsonIgnore
+    var enabled: Boolean? = null,
 
     @field:OneToMany(cascade = [(CascadeType.ALL)], orphanRemoval = true, mappedBy = "id.username", targetEntity = UserAuthority::class)
-    var authorities: Set<UserAuthority> = emptySet()
+    var authorities: Set<UserAuthority>? = emptySet()
 
 )
