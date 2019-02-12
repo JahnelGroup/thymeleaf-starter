7e06eaaeb0baac2ebe7f75b99811d19f71ec47c5
Steven Zgaljic <szgaljic@gmail.com>
2019-02-11 22:26:11 -0600

    finished admin create user form with password complexity

--- src/main/kotlin/com/jahnelgroup/controller/admin/form/CreateUserFormValidator.kt
+++ src/main/kotlin/com/jahnelgroup/controller/admin/form/CreateUserFormValidator.kt
@@ -1,17 +1,46 @@
 package com.jahnelgroup.controller.admin.form
 
-import com.jahnelgroup.domain.user.User
+import org.passay.*
 import org.springframework.validation.Errors
 import org.springframework.validation.ValidationUtils
 import org.springframework.validation.Validator
+import java.util.*
+
 
 class CreateUserFormValidator : Validator {
 
     override fun supports(clazz: Class<*>): Boolean {
-        return User::class.java == clazz
+        return CreateUserForm::class.java == clazz
     }
 
     override fun validate(createUserForm: Any, errors: Errors) {
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "", "Username must be provided.")
+        if(createUserForm is CreateUserForm){
+            arrayOf("username", "firstName", "lastName", "email", "password", "passwordConfrim").forEach {
+                ValidationUtils.rejectIfEmptyOrWhitespace(errors, it, "", "Field required.")
+            }
+
+            // Both passwords were provided
+            if( !errors.hasFieldErrors("password") && !errors.hasFieldErrors("passwordConfrim") ){
+
+                // passwords don't match
+                if( !createUserForm.password.equals(createUserForm.passwordConfrim) ){
+                    errors.rejectValue("password", "", "Passwords do not match.")
+                    errors.rejectValue("passwordConfrim", "", "Passwords do not match.")
+                }
+
+                // check complexity
+                else{
+                    // Validate password
+                    val passwordValidator = PasswordValidator(Arrays.asList(
+                            LengthRule(8, 30),
+                            RepeatCharacterRegexRule(RepeatCharacterRegexRule.MINIMUM_SEQUENCE_LENGTH),
+                            WhitespaceRule()))
+
+                    if( !passwordValidator.validate(PasswordData(createUserForm.password)).isValid ){
+                        errors.rejectValue("password", "", "Password does not meet the complexity requirements.")
+                    }
+                }
+            }
+        }
     }
 }
