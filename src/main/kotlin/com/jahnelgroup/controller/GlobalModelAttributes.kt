92b12d011fc96876150be3353c3526c16d46fc76
Steven Zgaljic <szgaljic@gmail.com>
2019-02-13 16:47:38 -0600

    conditional left nav links on settings page for non-current-user

--- src/main/kotlin/com/jahnelgroup/controller/GlobalModelAttributes.kt
+++ src/main/kotlin/com/jahnelgroup/controller/GlobalModelAttributes.kt
@@ -1,22 +1,16 @@
 package com.jahnelgroup.controller
 
 import com.jahnelgroup.service.context.UserContextService
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.context.request.ServletRequestAttributes
 
 @ControllerAdvice
 class ModelControllerAdvice(var userContextService: UserContextService){
 
+    @ModelAttribute("userContextService")
+    fun userContextService() = userContextService
+
     @ModelAttribute("currentUser")
     fun currentUser() = userContextService.currentUser()
-
-    @ModelAttribute("requestURI")
-    fun requestURI(model: Model): String{
-        val currentRequest = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request
-        return currentRequest.contextPath
-    }
 
 }
