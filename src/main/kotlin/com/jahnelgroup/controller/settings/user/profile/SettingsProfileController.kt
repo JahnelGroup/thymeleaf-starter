23839e99be3235db963419f70562989666e0e771
Steven Zgaljic <szgaljic@gmail.com>
2019-02-13 22:13:53 -0600

    Update profile working

--- src/main/kotlin/com/jahnelgroup/controller/settings/user/profile/SettingsProfileController.kt
+++ src/main/kotlin/com/jahnelgroup/controller/settings/user/profile/SettingsProfileController.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import javax.validation.Valid
@@ -28,15 +29,16 @@ class SettingsProfileController(
     @GetMapping("/settings/{user}/profile")
     fun profile(model: Model, @PathVariable user: String): String{
         // TODO: user may be null
-        model.addAttribute("user", userRepo.findByUsername(user).get())
+        val user = userRepo.findByUsername(user).get()
+        model.addAttribute("user", user)
+        model.addAttribute("updateProfileForm",
+                UpdateProfileForm(firstName = user.firstName, lastName = user.lastName))
         return "layouts/settings/user/profile"
     }
 
     @PostMapping("/settings/{user}/profile")
     fun updateProfile(model: Model, @PathVariable user: String,
             @Valid updateProfileForm: UpdateProfileForm, bindingResult: BindingResult): String{
-
-        model.mergeAttributes(model.asMap())
 
         if( !bindingResult.hasErrors() ){
             // TODO: user may be null
@@ -46,6 +48,11 @@ class SettingsProfileController(
             userRepo.save(user)
             model.addAttribute("updateProfileSuccessMessage", "Success!")
         }
+
+        // TODO: user may be null
+        val user = userRepo.findByUsername(user).get()
+        model.addAttribute("user", user)
+//        model.addAttribute("updateProfileForm", updateProfileForm)
 
         return "layouts/settings/user/profile"
     }
