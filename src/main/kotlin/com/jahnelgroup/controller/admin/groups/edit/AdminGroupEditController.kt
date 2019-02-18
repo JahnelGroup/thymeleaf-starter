da61c8e268647e6f182182cf9bd43a579f63c0af
Steven Zgaljic <szgaljic@gmail.com>
2019-02-17 21:51:53 -0600

    search groups

--- src/main/kotlin/com/jahnelgroup/controller/admin/groups/edit/AdminGroupEditController.kt
+++ src/main/kotlin/com/jahnelgroup/controller/admin/groups/edit/AdminGroupEditController.kt
@@ -1,5 +1,7 @@
 package com.jahnelgroup.controller.admin.groups.edit
 
+import com.jahnelgroup.domain.user.UserRepo
+import com.jahnelgroup.domain.user.group.GroupMemberRepo
 import com.jahnelgroup.domain.user.group.GroupRepo
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-class AdminGroupEditController(private var groupRepo: GroupRepo){
+class AdminGroupEditController(
+        private var groupRepo: GroupRepo,
+        private var userRepo: UserRepo
+){
 
     @GetMapping("/admin/groups/{groupId}")
     fun search(model: Model, @PathVariable groupId: Long): String {
         model.addAttribute("group", groupRepo.findById(groupId).get())
+        model.addAttribute("members", userRepo.findByGroupId(groupId))
         return "layouts/admin/groups/edit"
     }
 
